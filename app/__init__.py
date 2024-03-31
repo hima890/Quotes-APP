1b97f4db26b6c6bb6005ed9bc766e88adf2468a5
hima890 <hfibrahim90@gmail.com>
2024-03-31 14:42:03 +0200

    Starting the project main code layout, creat the app instance

---
+++ app/__init__.py
@@ -0,0 +1,7 @@
+"""
+__init__.py file serves as the entry point for the Flask application package.
+we call the libs that we need and create un instance of the flask main class called "quotes_app", set the config file and database connection settings and then return
+the app instance
+"""
+
+from flask import Flask
