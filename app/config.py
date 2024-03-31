0cde42c1302fad954e6239221b84c82afd85dd78
hima890 <hfibrahim90@gmail.com>
2024-03-31 15:07:36 +0200

    Updating the config file and create the .env file

---
+++ app/config.py
@@ -0,0 +1,29 @@
+"""
+Configuration settings for the Flask application.
+
+This file contains configuration variables used by the Flask app. These variables
+control various aspects of the application, such as database connection settings,
+security configurations, and other options.
+
+Attributes:
+    SECRET_KEY (str): Secret key used by Flask for cryptographic functions. It's
+        recommended to set this to a random string of characters to enhance security.
+    SQLALCHEMY_DATABASE_URI (str): URI for the database connection. This specifies
+        the database type, location, credentials, and database name.
+    DEBUG (bool): Flag indicating whether debug mode is enabled. Debug mode provides
+        detailed error messages and enables various debugging features. It should be
+        disabled in production environments.
+"""
+
+from dotenv import load_dotenv
+import os
+
+# Load environment variables from .env file
+load_dotenv()
+
+# The app secret key string for usnig cockies and sessions
+SECRET_KEY = os.environ.get('FLASK_SECRET_KEY')
+# The app database link ("if not created the flask app will create it")
+SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+# This logic var tell the flask app if we where in devolpment env or production env
+DEBUG = True
